2edeaf33558e020e226d48e895e1b7dd32d61ba2
Karim <karamanov2007@gmail.com>
2025-10-06 22:38:06 +0300

    add optimal sorting homework

---
+++ src/c_homework/optimalSorting/quickSort.c
@@ -0,0 +1,32 @@
+
+
+void swap(int* v1, int* v2)
+{
+    int temp = *v1;
+    *v1 = *v2;
+    *v2 = temp;
+}
+
+int partitation(int low, int high, int values[])
+{
+    int pivot = values[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; ++j) {
+        if (values[j] < pivot) {
+            i++;
+            swap(&values[i], &values[j]);
+        }
+    }
+    swap(&values[i + 1], &values[high]);
+    return i + 1;
+}
+
+void quickSort(int values[], int low, int high)
+{
+    if (low < high) {
+        int pi = partitation(low, high, values);
+        quickSort(values, pi + 1, high);
+        quickSort(values, low, pi - 1);
+    }
+}
---
+++ src/c_homework/optimalSorting/start.md
@@ -0,0 +1,8 @@
+# Для компиляции используйте команду:
+``` bash
+gcc optimalSorting.c quickSort.s -o program
+```
+# Запуск:
+```bash
+./program 
+```
