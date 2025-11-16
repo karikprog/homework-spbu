71b5609c0a77171783ce3ffd7d15db0268a45127
Karim <karamanov2007@gmail.com>
2025-11-16 18:41:06 +0300

    update extended_gcd_alg.py

--- src/introduction_to_git/extended_gcd_alg.py
+++ src/introduction_to_git/extended_gcd_alg.py
@@ -1,16 +1,14 @@
+def extended_gcd(a, b):
+    s0, s1 = 1, 0
+    t0, t1 = 0, 1
 
-def extended_gcd(num1, num2):
-    s0, t0 = 0, 0
-    s1, t1 = 1, 1
-    while num2 != 0:
-        q = num1 // num2
-        num1, num2, = num2, num1 % num2
-        s1, s0 = s0, s1 - q * s0
+    while b != 0:
+        q = a // b
+        a, b = b, a % b
+        s0, s1 = s1, s0 - q * s1
         t0, t1 = t1, t0 - q * t1
 
-    return (abs(num1), s0, t1)
+    return a, s0, t0
+
 
 print(extended_gcd(240, 46))
-
-
-
