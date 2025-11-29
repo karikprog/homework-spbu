6200b84927029041965d4ac254667a39d8d2a1ac
Karim <karamanov2007@gmail.com>
2025-11-29 17:59:07 +0300

    fix counting homework

---
+++ src/c_homework/counting/circularList.h
@@ -0,0 +1,20 @@
+#pragma once
+
+typedef struct CircularListNode {
+    int data;
+    struct CircularListNode* next;
+} CircularListNode;
+
+typedef struct {
+    int size;
+    struct CircularListNode* head;
+} CircularLinkedList;
+
+CircularListNode* createNode();
+CircularLinkedList* createCircularLinkedList();
+int isEmpty(CircularLinkedList* list);
+void addFirst(CircularLinkedList* list, int value);
+void addLast(CircularLinkedList* list, int value);
+void deleteValue(CircularLinkedList* list, int value);
+void deleteList(CircularLinkedList* list);
+void deleteNode(CircularLinkedList* list, CircularListNode* curr, CircularListNode* prev);
