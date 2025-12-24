990ac51b9c83ba7b834bd73095d45a65f28fe1ce
Karim <karamanov2007@gmail.com>
2025-12-24 17:46:04 +0300

    fix curry func

--- src/currying_homework/curry.py
+++ src/currying_homework/curry.py
@@ -1,4 +1,11 @@
 def curry(func, n: int):
+    if not (isinstance(n, int)):
+        raise TypeError("Number not int")
+    if n < 0:
+        raise ValueError("Value must be non-negative")
+    if n > func.__code__.co_argcount:
+        raise ValueError("Arity exceeds functions argument")
+
     def _carried(*args):
         if len(args) > n:
             raise TypeError("invalid amount argument")
@@ -10,6 +17,11 @@ def curry(func, n: int):
 
 
 def uncurry(func, n: int):
+    if not isinstance(n, int):
+        raise TypeError("arity is't integer")
+    if n < 0:
+        raise ValueError("arity must be non-negative")
+
     def _uncurried(*args):
         if len(args) != n:
             raise TypeError("invalid amount argument")
@@ -25,7 +37,8 @@ def sum_nums(x, y, z):
     return x * y * z
 
 
-carry_sum = curry(sum_nums, 3)
-uncurry_sum = uncurry(carry_sum, 3)
-print(carry_sum(1)(3)(9))
-print(uncurry_sum(10, 3, 4))
+if __name__ == "__main__":
+    carry_sum = curry(sum_nums, 3)
+    uncurry_sum = uncurry(carry_sum, 3)
+    print(carry_sum(1)(3)(9))
+    print(uncurry_sum(10, 3, 4))
